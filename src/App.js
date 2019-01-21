216d0baea9863d52099334c69c179fa717b7e99f
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-01-22 02:01:24 +0800

    [Input] build core components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Input, Button } from '@material-ui/core';
+import styled from 'styled-components';
+import * as Text from './constants/uiTexts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Header
         </header>
-      </div>
+        <Home>
+          <Section>
+            <InputField>
+              <TitleText>
+                Starting station
+              </TitleText>
+              <StationInput placeholder={Text.SEARCH_PLACEHOLDER}/>
+            </InputField>
+
+            <InputField>
+              <TitleText>
+                Destination station
+              </TitleText>
+              <StationInput placeholder={Text.SEARCH_PLACEHOLDER}/>
+            </InputField>
+
+            <Button variant="contained" fullWidth color="primary">
+              Go
+            </Button>
+          </Section>
+        </Home>
+      </React.Fragment>
     );
   }
 }
+
+const Home = styled.div`
+  text-align: center;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TitleText = styled.div`
+  text-align: left;
+  font-weight: bold;
+  margin: 16px 0;
+`;
+
+const StationInput = styled(Input)`
+  width: calc(100% - 16px);
+  border: solid gray .2px;
+  padding: 8px;
+`;
+
+const InputField = styled.div`
+  width: 70vw;
+  margin: 48px auto;
+`;
+
+const Section = styled.div`
+  background-color: lightblue;
+  padding: 16px;
+`;
 
 export default App;
